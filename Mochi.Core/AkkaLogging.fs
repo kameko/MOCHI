0e7b4ba5edef594169514b4b6b07d6fe766384e9
Kameko Kirishiki <kameko.k@outlook.com>
2019-10-07 16:25:31 -0400

    fiddling with akka logging

---
+++ Mochi.Core/AkkaLogging.fs
@@ -0,0 +1,35 @@
+﻿
+namespace Mochi.Core
+
+module AkkaLogging =
+    
+    open System
+    open System.Runtime.CompilerServices
+    open Akka.Actor
+    open Akka.Event
+    open Akka.FSharp
+    open Akka.Logger.Serilog
+    open Logging
+
+    let makeContext (sl : StructuredLog) (la : ILoggingAdapter) =
+        let mutable logger = la
+        logger <- logger.ForContext ("CallerName"       , sl.CallerName)
+        logger <- logger.ForContext ("CallerNamespace"  , sl.CallerNamespace)
+        logger <- logger.ForContext ("CallerFile"       , sl.CallerFile)
+        logger <- logger.ForContext ("CallerDirectory"  , sl.CallerDirectory)
+        logger <- logger.ForContext ("CallerLineNumber" , sl.CallerLineNumber)
+        logger
+
+    let akkalogger (mailbox : Actor<'Message>) =
+        let sl = StructuredLog.Create (1, String.Empty)
+        makeContext sl <| mailbox.Context.GetLogger ()
+
+    let akkaloggerm (mailbox : Actor<'Message>) (msg : string) =
+        let sl = StructuredLog.Create (1, msg)
+        makeContext sl <| mailbox.Context.GetLogger ()
+
+    // [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let akkaloggerem (mailbox : Actor<'Message>) (excp : Exception) (msg : string) =
+        let sl = StructuredLog.Create (1, msg, excp)
+        makeContext sl <| mailbox.Context.GetLogger ()
+        
--- Mochi.Tests/Program.fs
+++ Mochi.Tests/Program.fs
@@ -5,8 +5,12 @@ module Program =
     
     open System
     open System.Runtime.CompilerServices
+    open Akka.Actor
+    open Akka.FSharp
     open Serilog
+    open Akka.Logger.Serilog
     open Mochi.Core.Logging
+    open Mochi.Core.AkkaLogging
     
     let ``Test function logging stack frame depth`` _ =
         syslog.info "syslog.info top level"
@@ -43,6 +47,9 @@ module Program =
                 let guid2 = ((Guid.NewGuid ()).ToString()).ToUpper()
                 Console.WriteLine("{0} {1} ({2})", guid1, guid2, count)
                 count <- count + 1
+    
+    let ``Test actor message passing`` _ =
+        ()
 
     let commandReader _ =
         let mutable run = true
@@ -57,12 +64,13 @@ module Program =
                 let strindex = str.IndexOf(" ") + 1
                 let nstr = str.Substring strindex
                 match (strs.[0]).ToLower () with
+                | "info"                -> syslog.info nstr
                 | "warn"  | "warning"   -> syslog.warning nstr
                 | "error"               -> syslog.error nstr
                 | "fatal" | "critical"  -> syslog.fatal nstr
                 | "debug"               -> syslog.debug nstr
                 | "console"             -> Console.WriteLine nstr
-                | "info"                -> syslog.info nstr
+                | "rawlog"              -> Log.Information nstr
                 | _                     -> syslog.info str
             else if not (String.IsNullOrEmpty(str) || String.IsNullOrWhiteSpace(str)) then
                 match (strs.[0]).ToLower () with
@@ -70,30 +78,90 @@ module Program =
                 | "q"     -> ()
                 | "test1" -> ``Test function logging stack frame depth`` ()
                 | _       -> syslog.info str
+    
+    type Message =
+        | Die of string
+        | Msg of IActorRef * string
+
+    let commandActor system =
+        let aref = spawn system "command" (fun mailbox ->
+            let rec kurikaesu () = actor {
+                let! (message : obj) = mailbox.Receive()
+                match message with
+                | :? Message as msg ->
+                    match msg with
+                    | Msg (aref, str) -> 
+                        syslog.info <| sprintf "Got message: %s" str
+                        return! kurikaesu ()
+                    | Die str ->
+                        syslog.info <| sprintf "Shutting down. Reason: %s" str
+                | _ -> return! kurikaesu ()
+            }
+            kurikaesu ())
+        aref
+
+    let masterActor system =
+        let aref = spawn system "master" (fun mailbox ->
+            let rec kurikaesu () = actor {
+                let! (message : obj) = mailbox.Receive()
+                let logger = akkalogger mailbox
+                logger.Info <| sprintf "Got message: %O" message
+                return! kurikaesu ()
+            }
+            kurikaesu ())
+        aref
 
     let setupLogging _ =
         let mutable conf = LoggerConfiguration ()
-        conf <- conf.MinimumLevel.Debug()
+        conf <- conf.MinimumLevel.Debug() //.MinimumLevel.ControlledBy(levelSwitch)
         conf <- conf.WriteTo.Console (outputTemplate = 
             "[{Timestamp:HH:mm:ss.ff} {Level:u4}] " + 
             "[{CallerNamespace}.{CallerName} ({CallerFile}:{CallerLineNumber})]: " + 
             "{Message:lj}. {NewLine}{Exception}"
         )
+        //conf <- conf.Enrich.WithProperty("CallerNamespace", "Unknown")
+        conf <- conf.Enrich.WithProperty("CallerName", "Unknown")
         Log.Logger <- conf.CreateLogger ()
-        Mochi.Core.GCMonitor.start ()
         ()
+
+    let setupActors _ =
+        //let system = System.create "mochi" (Configuration.load())
+        let config = Configuration.parse "\
+            akka { \
+                loglevel=DEBUG, \
+                loggers=[\"Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog\"] \
+                log-config-on-start = on \
+                logtemplate = \"syslog {Message}\" \
+                actor { \
+                    debug { \
+                        receive = on \
+                        autoreceive = on \
+                        lifecycle = on \
+                        event-stream = on \
+                        unhandled = on \
+                    } \
+                } \
+            } \
+            "
+        let system = System.create "mochi" config
+        let master = masterActor system
+        master <! "yo wassap"
+        system
 
     [<MethodImpl(MethodImplOptions.NoInlining)>]
     let ``prelude pathos`` _ =
         setupLogging ()
         syslog.info "MOCHI Test Environment"
         syslog.info <| sprintf "Running in %s mode" (releaseString ())
+        Mochi.Core.GCMonitor.start ()
+        let system = setupActors ()
+        commandReader ()
+        system.Dispose ()
         ()
 
     [<EntryPoint>]
     let main _ =
         ``prelude pathos`` ()
-        commandReader ()
         0
 
 
